c728a83a0016e412b2117c6e5dddb4f8310c3e28
Samuel Peccoud <samuel@genofab.com>
2023-10-31 19:54:56 -0600

    Revert gain mods

--- teensy_edays_demo/ODriveInit.ino
+++ teensy_edays_demo/ODriveInit.ino
@@ -4,9 +4,9 @@
   //**************************************************************************
 
 void modifyGains() {  // this function turns up the gains when it is executed (menu option 4 via the remote)
-  float posGainKnee = 20.0/2;
-  float posGainHips = 60.0/2;
-  float posGainShoulder = 20.0/2;
+  float posGainKnee = 20.0;
+  float posGainHips = 60.0;
+  float posGainShoulder = 20.0;
   float velGain = 0.1;
   float integrator = 0.2;
 
