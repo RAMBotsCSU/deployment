1c0e9d6e4a25a1c8bd77cf402c31086b28f5f1c2
Samuel Peccoud <samuel@genofab.com>
2024-03-29 11:29:23 -0600

    Update offsets in config file

--- teensy_edays_demo/config.h
+++ teensy_edays_demo/config.h
@@ -8,17 +8,17 @@ float offSet50 = 1.39;  // KNEE
 
 // LEFT BACK LEG
 float offSet41 = -0.39;  // HIP
-float offSet61 = -0.35;  // SHOULDER
+float offSet61 = -0.85;  // SHOULDER
 float offSet60 = -1.10;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
 float offSet21 = -0.64;  // SHOULDER
-float offSet20 = -1.40;  // KNEE
+float offSet20 = -1.47;  // KNEE
 
 // RIGHT BACK LEG
 float offSet11 = 0.42;  // HIP
 float offSet31 = 0.45;  // SHOULDER
-float offSet30 = 1.65;  // KNEE
+float offSet30 = 1.30;  // KNEE
 
 #endif
