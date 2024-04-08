4611eed8f5cc3de347d99bedfeedc340d3fb64e3
Samuel Peccoud <samuel@genofab.com>
2024-04-08 16:21:41 -0600

    Remove lidar thread for test

--- pi_edays_demo/pi_edays_demo.py
+++ pi_edays_demo/pi_edays_demo.py
@@ -969,8 +969,8 @@ driver_thread = threading.Thread(target=driver_thread_funct, args=(controller,))
 driver_thread.daemon = True
 driver_thread.start()
 
-lidar_thread = threading.Thread(target=lidar_thread_funct, args=(controller,))
-lidar_thread.daemon = True
-lidar_thread.start()
+# lidar_thread = threading.Thread(target=lidar_thread_funct, args=(controller,))
+# lidar_thread.daemon = True
+# lidar_thread.start()
 
 controller.listen()
