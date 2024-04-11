76938e468a7a9daac42f55a9b59677af87791cb6
Samuel Peccoud <samuel@genofab.com>
2024-04-11 17:03:04 -0600

    Other lib fixes

--- pi_edays_demo/pi_edays_demo.py
+++ pi_edays_demo/pi_edays_demo.py
@@ -724,9 +724,8 @@ def lidar_thread_funct(controller):
         except Exception as e:
             print(e)
             time.sleep(1)
-            lidar.reset()
-            lidar.clear_input()
             lidar.stop()
+            lidar.stop_motor()
             lidar.disconnect()
             print("Lidar disconnected.")
 
