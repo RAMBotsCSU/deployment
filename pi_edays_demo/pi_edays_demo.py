5adce6e566269092e945515f8f094173e0deb3e7
McKenzy Johnson <101162938+mack99stack@users.noreply.github.com>
2024-04-13 15:33:14 -0600

    lidar thread

--- pi_edays_demo/pi_edays_demo.py
+++ pi_edays_demo/pi_edays_demo.py
@@ -17,8 +17,8 @@ import PySimpleGUI as sg
 import signal
 from math import cos, sin, pi
 import csv
-# from adafruit_rplidar import RPLidar
-from rplidar import RPLidar
+from adafruit_rplidar import RPLidar
+# from rplidar import RPLidar
 import queue
 from pycoral.utils import edgetpu
 import numpy as np
@@ -569,35 +569,35 @@ def driver_thread_funct(controller):
        # time.sleep(0.01)
         #update_gui_table_controller(controller)
 
-def setup_lidar_connection():
-    # Setup the RPLidar
-    PORT_NAME = '/dev/ttyUSB0'
+# def setup_lidar_connection():
+#     # Setup the RPLidar
+#     PORT_NAME = '/dev/ttyUSB0'
 
-    while True:
-        try:
-            # lidar = RPLidar(None, PORT_NAME, timeout=1)
-            # print("Lidar connected", lidar.info)
-            # # lidar.clear_input()
+#     while True:
+#         try:
+#             # lidar = RPLidar(None, PORT_NAME, timeout=1)
+#             # print("Lidar connected", lidar.info)
+#             # # lidar.clear_input()
 
-            lidar = RPLidar(PORT_NAME)
+#             lidar = RPLidar(PORT_NAME)
 
-            info = lidar.get_info()
-            print(info)
+#             info = lidar.get_info()
+#             print(info)
 
-            # health = lidar.get_health()
-            # print(health)
-            break
-        except Exception as e:
-            print(e)
-            print("Error connecting to lidar. Trying again")
-            time.sleep(1)
-    return lidar
+#             # health = lidar.get_health()
+#             # print(health)
+#             break
+#         except Exception as e:
+#             print(e)
+#             print("Error connecting to lidar. Trying again")
+#             time.sleep(1)
+#     return lidar
 
-global lidar
-lidar = setup_lidar_connection()
+# global lidar
+# lidar = setup_lidar_connection()
 
 def lidar_thread_funct(controller):
-    global lidar
+    # global lidar
     global lidar_view
     global STOP_FLAG
     # Set up pygame and the display
@@ -626,7 +626,7 @@ def lidar_thread_funct(controller):
 
     # Scale parameters to map
     scale_data = int(white_dot_threshold/map_width)
-  
+
     def update_lidar_map(data):
         lcd.fill((0,0,0))
         # Initialize a list to store Lidar data
@@ -679,56 +679,68 @@ def lidar_thread_funct(controller):
             temp_avg[angle_step] = dist_sum / (len(dist_buffer)*5) # average distance by size of dist_buffer
         return temp_avg
     
-    # lidar = setup_lidar_connection()
-    try:
-        for scan in lidar.iter_scans():
-        # for i, scan in enumerate(lidar.iter_scans()):
-            for (_, angle, distance) in scan:
-                scan_data[min([359, int(angle)])] = distance 
-            update_lidar_map(scan_data)
+    PORT_NAME = '/dev/ttyUSB0'
 
-            if controller.running_stop_mode:
+    def setup_lidar_connection():
+        while(True):
+            try:
+                lidar = RPLidar(None, PORT_NAME, timeout=3)
+                print("Lidar connected.")
+                break
+            except: 
+                print("Error connecting to lidar. Trying again")
 
-                if (time.time() - starttime) > 0.25: # every .25s
-                    starttime = time.time() # restart timer
-                    dist_buffer.append(scan_data) # add new data set to dist_buffer
-                    if len(dist_buffer) > window: # more data sets than window parameter
-                        dist_buffer.pop(0) # remove oldest data set
-                        avg_dist = update_avg_dist(dist_buffer) # get average of all data sets
-                        if min(avg_dist) <= red_dot_threshold: # any distance within stop proximity?
-                            STOP_FLAG = True
-                            print("Stop Proximity.")
-                        else:
-                            STOP_FLAG = False
+    lidar = setup_lidar_connection()
+    while(True):
+        try:
+            for scan in lidar.iter_scans():
+                for (_, angle, distance) in scan:
+                    scan_data[min([359, int(angle)])] = distance 
+                update_lidar_map(scan_data)
 
-        if controller.running_autonomous_walk:
+                if controller.running_stop_mode:
 
-            if time.time() - start_time > .2:
-                start_time = time.time()
-                lidar_view = scan_data
-                runLidarInference(scan_data, interpreter)
+                    if (time.time() - starttime) > 0.25: # every .25s
+                        starttime = time.time() # restart timer
+                        dist_buffer.append(scan_data) # add new data set to dist_buffer
+                        if len(dist_buffer) > window: # more data sets than window parameter
+                            dist_buffer.pop(0) # remove oldest data set
+                            avg_dist = update_avg_dist(dist_buffer) # get average of all data sets
+                            if min(avg_dist) <= red_dot_threshold: # any distance within stop proximity?
+                                STOP_FLAG = True
+                                print("Stop Proximity.")
+                            else:
+                                STOP_FLAG = False
 
-                joystickArr[0] = joystick_map_to_range(controller.l3_horizontal)+1
-                joystickArr[1] = joystick_map_to_range(controller.l3_vertical)+1
-                joystickArr[2] = trigger_map_to_range(controller.triggerL)+1
-                joystickArr[3] = joystick_map_to_range(controller.r3_horizontal)+1
-                joystickArr[4] = joystick_map_to_range(controller.r3_vertical)+1
-                joystickArr[5] = trigger_map_to_range(controller.triggerR)+1
+            if controller.running_autonomous_walk:
 
-                lidar_data.append(scan_data + joystickArr)
+                if time.time() - start_time > .2:
+                    start_time = time.time()
+                    lidar_view = scan_data
+                    runLidarInference(scan_data, interpreter)
 
-        elif len(lidar_data) > 0:
-            with open(output_file, 'w', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile)
-                csv_writer.writerows(lidar_data)
-            print(f'Lidar data saved to {output_file}')
-            lidar_data = []
-    except Exception as e:
-        print(e)
-    finally:
-        lidar.stop()
-        lidar.disconnect()
-        print("Lidar stopped.")
+                    joystickArr[0] = joystick_map_to_range(controller.l3_horizontal)+1
+                    joystickArr[1] = joystick_map_to_range(controller.l3_vertical)+1
+                    joystickArr[2] = trigger_map_to_range(controller.triggerL)+1
+                    joystickArr[3] = joystick_map_to_range(controller.r3_horizontal)+1
+                    joystickArr[4] = joystick_map_to_range(controller.r3_vertical)+1
+                    joystickArr[5] = trigger_map_to_range(controller.triggerR)+1
+
+                    lidar_data.append(scan_data + joystickArr)
+
+            elif len(lidar_data) > 0:
+                with open(output_file, 'w', newline='') as csvfile:
+                    csv_writer = csv.writer(csvfile)
+                    csv_writer.writerows(lidar_data)
+                print(f'Lidar data saved to {output_file}')
+                lidar_data = []
+        except Exception as e:
+            print(e)
+            lidar = setup_lidar_connection()
+        # finally:
+        #     lidar.stop()
+        #     lidar.disconnect()
+        #     print("Lidar stopped.")
 
 
 class MyController(Controller):
