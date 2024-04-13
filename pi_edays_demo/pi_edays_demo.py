aa8c02a16ae72a37ab64a3bd96f2a68e8583a09d
McKenzy Johnson <101162938+mack99stack@users.noreply.github.com>
2024-04-13 11:06:16 -0600

    Lidar edit

--- pi_edays_demo/pi_edays_demo.py
+++ pi_edays_demo/pi_edays_demo.py
@@ -675,51 +675,57 @@ def lidar_thread_funct(controller):
             temp_avg[angle_step] = dist_sum / (len(dist_buffer)*5) # average distance by size of dist_buffer
         return temp_avg
     
-    
-    lidar = setup_lidar_connection()
     try:
-        for scan in lidar.iter_scans():
-            for (_, angle, distance) in scan:
-                scan_data[min([359, int(angle)])] = distance 
-            update_lidar_map(scan_data)
+        while(True):
+            lidar = setup_lidar_connection()
+            try:
+                for scan in lidar.iter_scans():
+                    for (_, angle, distance) in scan:
+                        scan_data[min([359, int(angle)])] = distance 
+                    update_lidar_map(scan_data)
 
-            if controller.running_stop_mode:
+                    if controller.running_stop_mode:
 
-                if (time.time() - starttime) > 0.25: # every .25s
-                    starttime = time.time() # restart timer
-                    dist_buffer.append(scan_data) # add new data set to dist_buffer
-                    if len(dist_buffer) > window: # more data sets than window parameter
-                        dist_buffer.pop(0) # remove oldest data set
-                        avg_dist = update_avg_dist(dist_buffer) # get average of all data sets
-                        if min(avg_dist) <= red_dot_threshold: # any distance within stop proximity?
-                            STOP_FLAG = True
-                            print("Stop Proximity.")
-                        else:
-                            STOP_FLAG = False
+                        if (time.time() - starttime) > 0.25: # every .25s
+                            starttime = time.time() # restart timer
+                            dist_buffer.append(scan_data) # add new data set to dist_buffer
+                            if len(dist_buffer) > window: # more data sets than window parameter
+                                dist_buffer.pop(0) # remove oldest data set
+                                avg_dist = update_avg_dist(dist_buffer) # get average of all data sets
+                                if min(avg_dist) <= red_dot_threshold: # any distance within stop proximity?
+                                    STOP_FLAG = True
+                                    print("Stop Proximity.")
+                                else:
+                                    STOP_FLAG = False
 
-        if controller.running_autonomous_walk:
+                if controller.running_autonomous_walk:
 
-            if time.time() - start_time > .2:
-                start_time = time.time()
-                lidar_view = scan_data
-                runLidarInference(scan_data, interpreter)
+                    if time.time() - start_time > .2:
+                        start_time = time.time()
+                        lidar_view = scan_data
+                        runLidarInference(scan_data, interpreter)
 
-                joystickArr[0] = joystick_map_to_range(controller.l3_horizontal)+1
-                joystickArr[1] = joystick_map_to_range(controller.l3_vertical)+1
-                joystickArr[2] = trigger_map_to_range(controller.triggerL)+1
-                joystickArr[3] = joystick_map_to_range(controller.r3_horizontal)+1
-                joystickArr[4] = joystick_map_to_range(controller.r3_vertical)+1
-                joystickArr[5] = trigger_map_to_range(controller.triggerR)+1
+                        joystickArr[0] = joystick_map_to_range(controller.l3_horizontal)+1
+                        joystickArr[1] = joystick_map_to_range(controller.l3_vertical)+1
+                        joystickArr[2] = trigger_map_to_range(controller.triggerL)+1
+                        joystickArr[3] = joystick_map_to_range(controller.r3_horizontal)+1
+                        joystickArr[4] = joystick_map_to_range(controller.r3_vertical)+1
+                        joystickArr[5] = trigger_map_to_range(controller.triggerR)+1
 
-                lidar_data.append(scan_data + joystickArr)
+                        lidar_data.append(scan_data + joystickArr)
 
-        elif len(lidar_data) > 0:
-            with open(output_file, 'w', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile)
-                csv_writer.writerows(lidar_data)
-            print(f'Lidar data saved to {output_file}')
-            lidar_data = []
-
+                elif len(lidar_data) > 0:
+                    with open(output_file, 'w', newline='') as csvfile:
+                        csv_writer = csv.writer(csvfile)
+                        csv_writer.writerows(lidar_data)
+                    print(f'Lidar data saved to {output_file}')
+                    lidar_data = []
+            except Exception as E:
+                print(e)
+                lidar.reset()
+                lidar.clear_input()
+                lidar.stop()
+                lidar.disconnect()
     except KeyboardInterrupt:
         time.sleep(1)
         lidar.stop()
