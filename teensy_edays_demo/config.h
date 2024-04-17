008f4c2d4967abffc2fc6c1cf012b7f578eb4306
Samuel Peccoud <samuel@genofab.com>
2024-04-16 18:09:29 -0600

    Back legs struggling to walk commiting to revert

--- teensy_edays_demo/config.h
+++ teensy_edays_demo/config.h
@@ -2,23 +2,23 @@
 #define CONFIG_H
 
 // LEFT FRONT LEG
-float offSet40 = 0.40;  // HIP
-float offSet51 = 0.95;  // SHOULDER
-float offSet50 = 1.39;  // KNEE
+float offSet40 = 0.37;  // HIP
+float offSet51 = 1.01;  // SHOULDER
+float offSet50 = 1.62;  // KNEE
 
 // LEFT BACK LEG
-float offSet41 = -0.39;  // HIP
-float offSet61 = -0.85;  // SHOULDER
+float offSet41 = -0.35;  // HIP
+float offSet61 = -0.93;  // SHOULDER
 float offSet60 = -1.10;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
-float offSet21 = -0.64;  // SHOULDER
-float offSet20 = -1.47;  // KNEE
+float offSet21 = -0.77;  // SHOULDER
+float offSet20 = -1.80;  // KNEE
 
 // RIGHT BACK LEG
 float offSet11 = 0.42;  // HIP
-float offSet31 = 0.45;  // SHOULDER
-float offSet30 = 1.30;  // KNEE
+float offSet31 = 0.54;  // SHOULDER
+float offSet30 = 1.60;  // KNEE
 
 #endif
