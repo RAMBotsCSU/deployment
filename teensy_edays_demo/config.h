4f5f78ad90a6107ecb3a9e8bd9ab0b31b4b4c9cd
Samuel Peccoud <samuel@genofab.com>
2024-04-18 17:05:37 -0600

    Update config

--- teensy_edays_demo/config.h
+++ teensy_edays_demo/config.h
@@ -4,21 +4,21 @@
 // LEFT FRONT LEG
 float offSet40 = 0.37;  // HIP
 float offSet51 = 1.01;  // SHOULDER
-float offSet50 = 1.62;  // KNEE
+float offSet50 = 0.95;  // KNEE
 
 // LEFT BACK LEG
 float offSet41 = -0.35;  // HIP
 float offSet61 = -0.93;  // SHOULDER
-float offSet60 = -1.10;  // KNEE
+float offSet60 = -1.30;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
 float offSet21 = -0.77;  // SHOULDER
-float offSet20 = -1.80;  // KNEE
+float offSet20 = -1.00;  // KNEE
 
 // RIGHT BACK LEG
 float offSet11 = 0.42;  // HIP
 float offSet31 = 0.54;  // SHOULDER
-float offSet30 = 1.60;  // KNEE
+float offSet30 = 1.05;  // KNEE
 
 #endif
