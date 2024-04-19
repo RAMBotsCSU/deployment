a1707d7a9056667cb9de124f6088dce705cf0571
ersmith01 <elaines@colostate.edu>
2024-04-19 08:08:05 -0600

    updating offsets

--- teensy_edays_demo/config.h
+++ teensy_edays_demo/config.h
@@ -3,21 +3,21 @@
 
 // LEFT FRONT LEG
 float offSet40 = 0.40;  // HIP
-float offSet51 = 0.85;  // SHOULDER
-float offSet50 = 0.79;  // KNEE
+float offSet51 = 1.00;  // SHOULDER
+float offSet50 = 1.39;  // KNEE
 
 // LEFT BACK LEG
-float offSet41 = -0.39;  // HIP
-float offSet61 = -0.77;  // SHOULDER
-float offSet60 = -1.00;  // KNEE
+float offSet41 = -0.37;  // HIP
+float offSet61 = -0.95;  // SHOULDER
+float offSet60 = -1.60;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
-float offSet21 = -0.62;  // SHOULDER
-float offSet20 = -0.77;  // KNEE
+float offSet21 = -0.78;  // SHOULDER
+float offSet20 = -1.47;  // KNEE
 
 // RIGHT BACK LEG
-float offSet11 = 0.42;  // HIP
-float offSet31 = 0.38;  // SHOULDER
-float offSet30 = 0.80;  // KNEE
+float offSet11 = 0.39;  // HIP
+float offSet31 = 0.55;  // SHOULDER
+float offSet30 = 1.40;  // KNEE
 #endif
