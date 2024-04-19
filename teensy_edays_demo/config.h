c4235df44dae37d984e3fd9d8ca999f026a9aee2
ersmith01 <elaines@colostate.edu>
2024-04-18 23:37:25 -0600

    adding new positions to legs

--- teensy_edays_demo/config.h
+++ teensy_edays_demo/config.h
@@ -2,23 +2,22 @@
 #define CONFIG_H
 
 // LEFT FRONT LEG
-float offSet40 = 0.37;  // HIP
-float offSet51 = 1.01;  // SHOULDER
-float offSet50 = 0.95;  // KNEE
+float offSet40 = 0.40;  // HIP
+float offSet51 = 0.85;  // SHOULDER
+float offSet50 = 0.79;  // KNEE
 
 // LEFT BACK LEG
-float offSet41 = -0.35;  // HIP
-float offSet61 = -0.93;  // SHOULDER
-float offSet60 = -1.30;  // KNEE
+float offSet41 = -0.39;  // HIP
+float offSet61 = -0.77;  // SHOULDER
+float offSet60 = -1.00;  // KNEE
 
 // RIGHT FRONT LEG
 float offSet10 = -0.47;  // HIP
-float offSet21 = -0.77;  // SHOULDER
-float offSet20 = -1.00;  // KNEE
+float offSet21 = -0.62;  // SHOULDER
+float offSet20 = -0.77;  // KNEE
 
 // RIGHT BACK LEG
 float offSet11 = 0.42;  // HIP
-float offSet31 = 0.54;  // SHOULDER
-float offSet30 = 1.05;  // KNEE
-
+float offSet31 = 0.38;  // SHOULDER
+float offSet30 = 0.80;  // KNEE
 #endif
